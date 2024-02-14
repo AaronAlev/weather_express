08c3808123bfd13708c58b9015cb253c4926bafd
Lugeja <aaronalev@gmail.com>
2024-02-14 13:45:39 +0200

    Created form request responce with API data

--- index.js
+++ index.js
@@ -6,10 +6,31 @@ const fetch = require('node-fetch');
 const key = '0f84daf1e95eeaf07d5e114f93eb376a';
 let city = 'Tartu'
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', function (req, res) {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        let description = data.weather[0].description;
+        let city = data.name;
+        let temp = Math.round(parseFloat(data.main.temp) - 273.15);
+        res.render('index', {
+            description: description,
+            city: city,
+            temp: temp
+        })
+    });
+});
+
+app.post('/', function (req, res) {
+    let city = req.body.cityname;
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
     .then((response) => {
         return response.json();
